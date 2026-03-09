6229a6406fc3d95cec6b4cbd2cb186d36d162067
Okan Kaya <ok.kaya.thuis@gmail.com>
2026-03-09 15:41:51 +0100

    feat: connect action panel to recommendation data

--- src/features/actions/ActionPanel.jsx
+++ src/features/actions/ActionPanel.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import SectionCard from '../../components/SectionCard.jsx'
+import { mockParticipant } from '../../data/mockParticipant'
 
 function ActionPanel() {
+  const recommendations = mockParticipant.recommendations
+
   return (
     <SectionCard title="Preventive Action Recommendations">
       <ul>
-        <li>Keep a regular bedtime for the next 7 days.</li>
-        <li>Add one 20-minute walk after lunch, 3 times this week.</li>
-        <li>Complete the next check-in before Friday.</li>
+        {recommendations.map((recommendation, index) => (
+          <li key={index}>{recommendation}</li>
+        ))}
       </ul>
     </SectionCard>
   )
